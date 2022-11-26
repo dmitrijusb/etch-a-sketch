c56d5edf806eb79ed6fb95845f172210ae017c60
Dmitrijus <dmitrijus.byckovas@stud.svako.lt>
2022-11-26 14:21:41 +0200

    Codea clean up

--- script/script.js
+++ script/script.js
@@ -1,24 +1,27 @@
+"use strict";
+
 //Queries
 const btnGrid = document.querySelector(".btn-grid-size");
 const btnReset = document.querySelector(".btn-grid-reset");
 const gridContainer = document.querySelector(".grid-container");
 const gridContainerSize = document.querySelector(".grid-container-size");
+const btnRgb = document.querySelector(".btn-rgb");
+
 //Variables
-let cells;
-console.log(isNaN("5"));
+let cells, color, counter;
+let rgb = false;
 
 //Functions
 
 //Function to resize the grid
-function resizeGrid() {
-  //Prompt to enter the Grid Size
+function setGridSize() {
+  //Checking if container already has any of the div elements (avoid stacking of previously used size)
+  if (gridContainer.firstChild) gridContainer.innerHTML = "";
 
+  //Prompt to enter the Grid Size
   do {
     cells = prompt("Enter number of squares per side:");
   } while (cells < 0 || cells > 100 || isNaN(cells));
-
-  //Checking if container already has any of the div elements (avoid stacking of previously used size)
-  if (gridContainer.firstChild) gridContainer.innerHTML = "";
 
   //Setting up the size
   gridContainerSize.setAttribute(
@@ -26,39 +29,50 @@ function resizeGrid() {
     `grid-template-columns: repeat(${cells}, 1fr); grid-template-rows: repeat(${cells}, 1fr)`
   );
 
+  //Creating the divs
   cells *= cells;
-
   for (let i = 0; i < cells; i++) {
     const div = document.createElement("div");
     div.classList.add("cell");
     gridContainer.append(div);
   }
 
-  const cell = document.querySelectorAll(".cell");
-
-  for (let i = 0; i < cell.length; i++) {
-    cell[i].addEventListener("mouseover", colorCell);
-  }
+  //Calling coloring function
+  colorGrid();
 }
 
 //Function to reset the grid
 function resetGrid() {
-  cells = 0;
-  gridContainerSize.removeAttribute(
-    "style",
-    `grid-template-columns: repeat(${cells}, 1fr); grid-template-rows: repeat(${cells}, 1fr)`
-  );
-  gridContainer.innerHTML = "";
+  document.querySelectorAll(".cell").forEach((c) => c.removeAttribute("style"));
+}
+
+//Function to color the grid
+function colorGrid() {
+  const cell = document.querySelectorAll(".cell");
+  counter = 0;
+
+  for (let i = 0; i < cell.length; i++) {
+    cell[i].addEventListener("mouseover", function (e) {
+      if (rgb) color = getRandomRgb();
+      else color = "black";
+      e.target.style.background = color;
+    });
+  }
+}
+
+//Function to generate rgb value
+function getRandomRgb() {
+  var num = Math.round(0xffffff * Math.random());
+  var r = num >> 16;
+  var g = (num >> 8) & 255;
+  var b = num & 255;
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 //DOM
-btnGrid.addEventListener("click", resizeGrid);
+btnGrid.addEventListener("click", setGridSize);
 btnReset.addEventListener("click", resetGrid);
-// cell.addEventListener("mouseover", function (e) {
-//   console.log(e);
-// });
-
-function colorCell(e) {
-  console.log(e);
-  e.target.style.background = "black";
-}
+btnRgb.addEventListener("click", function () {
+  rgb = !rgb;
+  btnRgb.classList.toggle("btn-rgb-active");
+});
